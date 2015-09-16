a6af214216db40ae7e61d86a984bbe9a36886527
Farzan <pharzan@outlook.com>
2015-09-16 16:34:10 +0300

    Create instatinde.blade.php

---
+++ instatinde.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>instaTinder</title>
+    @include('instatinder.includes.head')
+</head>
+
+<body>
+<div class="container">
+    <div id="row">
+        <div id="instatinder" class="col-md-8"></div>
+        <div id="sidebar" class="col-md-4"></div>
+
+    </div>
+</div>
+@include('instatinder.includes.footer')
+
+<script src="assets/js/instatinder.js"></script>
+
+</body>
+
+</html>
