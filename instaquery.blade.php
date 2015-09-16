9a7b1fca93bd0f66bf59cfc5884786dd46edec4b
Farzan <pharzan@outlook.com>
2015-09-16 16:32:22 +0300

    Create instaquery.blade.php

---
+++ instaquery.blade.php
@@ -0,0 +1,32 @@
+<?php
+
+require 'assets/lib/instagram/insta.php';
+use MetzWeb\Instagram\Instagram;
+
+$QR = 'No query';
+$result = array();
+$image = array();
+$NumberOfInstaPosts = 19;
+$instagram = new Instagram(array(
+        'apiKey' => '263fd9c7bed64263854c344f91fd7ec6',
+        'apiSecret' => 'd1d1b18178a84c7a964e7bb5e155bc4d',
+        'apiCallback' => 'http://www.pharzan.com/insta_success_callback.php' // must point to success.php
+));
+if (isset($_GET['type'])) {
+    if ($_GET['type'] == 'selfie') {
+
+
+        $result[0] = $instagram->getTagMedia('selfie');
+
+        for ($i = 0; $i <= $NumberOfInstaPosts - 1; $i++) {
+            $image['links'][$i] = $result[0]->data[$i]->images->standard_resolution->url;
+            $image['userinfo'][$i] = $result[0]->data[$i]->caption;
+        }
+
+        //print_r($image);
+        $QR = $image;
+    }
+
+}
+
+echo json_encode($QR);
